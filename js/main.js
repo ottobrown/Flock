047306323c4908083bc3dba4d90b6963e378dfd5
ottobrown <77187512+ottobrown@users.noreply.github.com>
2021-04-25 11:53:48 -0600

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,21 @@
+import { creatures } from './creature.js'
+const canv = document.getElementById('mycanvas')
+const ctx = canv.getContext('2d')
+const fps = 60 // Edit this to change game fps
+
+let lastframe = 0
+function loop(currentTime) {
+    window.requestAnimationFrame(loop)
+    let frameTime = ((currentTime) - lastframe) / 1000
+    if (frameTime < 1/fps) {return}
+    lastframe = currentTime
+
+    ctx.clearRect(0, 0, canv.width, canv.height) //Clears the canvas each frame
+
+    creatures.forEach(element => {
+        element.Frame()
+    });
+    //Put all functions that run on each frame here
+}
+
+loop()
